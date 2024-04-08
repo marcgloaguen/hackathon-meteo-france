c6d3d17d4bdaecb0e8a7f8154efd901668db1f28
Marc GLOAGUEN <marc.gloaguen@gmail.com>
2024-04-08 18:11:21 +0200

    Revert "Marc"
    This reverts commit 14df54ab
    
    Signed-off-by: Marc GLOAGUEN <marc.gloaguen@gmail.com>

--- module.py
+++ module.py
@@ -3,6 +3,26 @@ from langchain_openai import OpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 import os
+from dotenv import load_dotenv
+import time
+import os
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.messages import HumanMessage
+from langchain_community.vectorstores import Chroma
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+load_dotenv()
+
+with open("contextualize_system_prompt.txt", "r") as files:
+    contextualize_system_prompt = files.read()
+
+with open("instruct.txt", "r") as files:
+    instruct = files.read()
 
 
 def extract_text_items(data):
@@ -47,3 +67,49 @@ def summarize_news(llm, news: list) -> str:
     )
     summary = chain.invoke(news)
     return summary
+
+
+def create_retriever(path: str = "data/Vigilance Table.csv"):
+    loader = CSVLoader(file_path=path)
+    data = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_documents(data)
+    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+    retriever = vectorstore.as_retriever()
+    return retriever
+
+
+def contextualize_chain(llm):
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_system_prompt),
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("human", "{question}"),
+        ]
+    )
+    chain = prompt | llm | StrOutputParser()
+    return chain
+
+
+def rag_chain_with_history(llm, retriever):
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", instruct),
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("human", "{question}"),
+        ]
+    )
+
+    def contextualized_question(input: dict):
+        if input.get("chat_history"):
+            return contextualize_chain(llm)
+        else:
+            return input["question"]
+
+    chain = (
+            RunnablePassthrough.assign(context=contextualized_question | retriever)
+            | prompt
+            | llm
+    )
+
+    return chain
